6782eb2ce9ea12386bd4fa7b3c99f283751f0347
Milakrei55 <mischakom@gmail.com>
2025-01-21 11:33:51 +0100

    Adding new iterations of code (loop to write out names, biggest pokemon)

--- pokedex-app/js/scripts.js
+++ pokedex-app/js/scripts.js
@@ -1,5 +1,32 @@
+// Pokémon array
 let pokemonList = [
-  { name: 'Treecko', height: 0.5, type: 'Grass'},
-  { name: 'Cyndaquil', height: 0.5, type: 'Fire'},
-  { name: 'Dratini', height: 1.8, type: 'Dragon'}
+  {
+    name: 'Treecko',
+    height: 0.5,
+    type: ['Grass']
+  },
+  {
+    name: 'Cyndaquil',
+    height: 0.5,
+    type: ['Fire']
+  },
+  {
+    name: 'Dratini',
+    height: 1.8,
+    type: ['Dragon']
+  },
 ];
+
+// Loop code to write the Pokémon names + biggest Pokémon
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1) {
+    message = "- Wow, that's big!";
+  } else {
+    message = "";
+  }
+  document.write(
+    `${pokemonList[i].name} (height: ${pokemonList[i].height})
+      ${message} 
+      <br>`
+  );
+}
