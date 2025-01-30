f4fd6c44ed997158aaab2fbaf00bb4422600a37a
Milakrei55 <mischakom@gmail.com>
2025-01-30 10:28:31 +0100

    Updates with loading all of the Pokemon

--- pokedex-app/js/scripts.js
+++ pokedex-app/js/scripts.js
@@ -1,83 +1,82 @@
-// Pokémon array
+// Declaration of Pokémon array
 let pokemonRepository = (function () {
-    let pokemonList = [
-        {
-            name: 'Treecko',
-            height: 0.5,
-            types: ['Grass']
-        },
-        {
-            name: 'Cyndaquil',
-            height: 0.5,
-            types: ['Fire']
-        },
-        {
-            name: 'Dratini',
-            height: 1.8,
-            types: ['Dragon']
+    let pokemonList = [];
+    let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+    function add(pokemon) {
+        if (
+            typeof pokemon === "object" &&
+            "name" in pokemon &&
+            "detailsURL" in pokemon
+        ) {
+            pokemonList.push(pokemon);
+        } else {
+            console.log("Pokemon is not correct");
         }
-    ];
-    // create pokemonList accessible outside of the function
+    }
+
     function getAll() {
         return pokemonList;
     }
-    // add Pokémon if conditions are met
-    function add(addPokemon) {
-        let keysNeeded = ['name', 'height', 'type'];
-        if (
-            typeof addPokemon === 'object' && // checks if item is an object
-            Object.keys(addPokemon).length === keysNeeded.length && // checks if amount of keys in item is the same as the amount defined in keysNeeded
-            addPokemon !== null && // prevent typeof null === 'object' quirk
-            addPokemon.name !== undefined && // checks if name is defined
-            addPokemon.height !== undefined && //checks if height is defined
-            addPokemon.type !== undefined // checks if type is defined
-        ) {
-            pokemonList.push(addPokemon); // add item if expectations are met
-        } else {
-            console.error('Please provide an object with name, height and type properties') // print error in console, if expectations were not met
-        }
+
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listPokemon = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        button.addEventListener('click', function () {
+            showDetails(pokemon);
+        });
+        listPokemon.appendChild(button);
+        pokemonList.appendChild(listPokemon);
     }
-    // Function to add event listener to button
-    function addListenerToButton(button, pokemon) {
-        button.addEventListener('click', function () { // Event listener, that listens to 'click'
-            showDetails(pokemon); // Call showDetails()-function with pokemon as parameter
+    function showDetails(pokemon) {
+        loadDetails(pokemon).then(function () {
+            console.log(pokemon);
         });
     }
-    // Function to create list items with Pokémon details including a button that displays the name
-    function addListItem(pokemon) {
-        let pokemonsList = document.querySelector('.pokemon-list');
-        let listItemPokemon = document.createElement('li');
-        let button = document.createElement('button');
 
-        button.innerText = pokemon.name; // Text of Button is = name of Pokémon
-        button.classList.add('button-class'); // Add a class to the button for easier styling
-        addListenerToButton(button, pokemon); // function addListenerTo Button is called and passed with the 2 arguments (button, pokemon)
-
-        listItemPokemon.appendChild(button); // Append button to list item 
-        pokemonsList.appendChild(listItemPokemon); // Append list item to ul
+    function loadList() {
+        return fetch(apiURL).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsURL: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
     }
-
-    // Function to show Pokémon details (pokemon as argument)
-    function showDetails(pokemon) {
-        console.log(pokemon);
+    function loadDetails(item) {
+        let url = item.detailsURL;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            item.imageURL = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e) {
+            console.error(e)
+        });
     }
-
-    // make them accessible from outside of the function
     return {
-        getAll: getAll,
         add: add,
+        getAll: getAll,
         addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
         showDetails: showDetails
-    }
+    };
 })();
-// filter() - function : filter by name
-function findName(nameList, nameSearched) {
-    return nameList.filter((addPokemon) =>
-        addPokemon.name.toLowerCase().includes(nameSearched.toLowerCase())
-    );
-}
-pokemonRepository.getAll().forEach(pokemonRepository.addListItem);
 
-
-// pokemonRepository.add({name: 'Butterfree', height: 1.1, type: ['bug','flying'] }); // check if add() works
-// console.log(findName(pokemonRepository.getAll(), 'pidgey')); // check if filter() works
+// ForEach method to print out details of each Pokémon
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
+});
